e1e134e0238459d6678334ef977e0ccc3c93851e
Malik Laet <isklikma@gmail.com>
2022-12-28 19:25:47 -0300

    frist commit

---
+++ js-consumindo-dados-api-main/main.js
@@ -0,0 +1,29 @@
+async function buscaEndereco(cep) {
+    let mensagemErro = document.getElementById("erro")
+    mensagemErro.innerHTML = "";
+    try {
+        let consultaCEP = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
+        let consultaCEPConvertida = await consultaCEP.json()
+        if (consultaCEPConvertida.erro) {
+            throw Error("cep invalido")
+        }
+        let endereco = document.getElementById("endereco");
+        let bairro = document.getElementById("bairro");
+        let cidade = document.getElementById("cidade");
+        let estado = document.getElementById("estado");
+
+        cidade.value = consultaCEPConvertida.localidade;
+        estado.value = consultaCEPConvertida.uf;
+        endereco.value = consultaCEPConvertida.logradouro;
+        bairro.value = consultaCEPConvertida.bairro
+
+        console.log(consultaCEPConvertida);
+        return consultaCEPConvertida;
+    } catch (erro) {
+        mensagemErro.innerHTML =  `<p>CEP inválido. Tente novamente!</p>`
+        console.log(erro)
+    }
+}
+
+let cep = document.getElementById('cep')
+cep.addEventListener("focusout", () => buscaEndereco(cep.value))
